394450d7a9de4cc47aea0c4a75412c133cbfd06f
n2one <kubawojtasik70@gmail.com>
2018-10-19 19:31:12 +0200

    Init git and Selection Sort C++

---
+++ Selection Sort/C++/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<array>
+void Sort(int length,int tab[]){
+  for(int i=0;i < length;i++){
+    int index=-1;
+    int smallest = tab[i];
+    for (int j = i; j < length; j++) {
+      if(tab[j] < smallest){
+        std::cout << tab[j]<<"->"<<smallest<<"\n";
+        smallest = tab[j];
+        index=j;
+      }
+    }
+    int temp = tab[i];
+    tab[i]=smallest;
+    tab[index]=temp;
+  }
+  for (int i = 0; i < length; i++) {
+      std::cout << tab[i]<<" ";
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  int a[8]={2,3,5,4,10,-10,1,4};
+  Sort(8,a);
+  return 0;
+}
